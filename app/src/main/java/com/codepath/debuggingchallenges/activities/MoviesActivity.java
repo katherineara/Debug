ca5eadcf4fcbe81314b9fcef83b2fabfce0974d1
Katherine Arackaparambil <katherineara@fb.com>
2018-07-10 11:35:11 -0700

    Initial commit

---
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/MoviesActivity.java
@@ -0,0 +1,70 @@
+package com.codepath.debuggingchallenges.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.codepath.debuggingchallenges.R;
+import com.codepath.debuggingchallenges.adapters.MoviesAdapter;
+import com.codepath.debuggingchallenges.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MoviesActivity extends AppCompatActivity {
+
+    private static final String API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+    ListView lvMovies;
+    MoviesAdapter adapter;
+    ArrayList<Movie> movies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+        lvMovies = (ListView) findViewById(R.id.lvMovies);
+
+        movies = new ArrayList<>();
+        // Create the adapter to convert the array to views
+        adapter = new MoviesAdapter(this, movies);
+
+        // Attach the adapter to a ListView
+        lvMovies.setAdapter(adapter);
+
+        fetchMovies();
+
+    }
+
+    private void fetchMovies() {
+        RequestParams params = new RequestParams();
+        params.put(null, API_KEY);
+        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + API_KEY;
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONArray moviesJson = response.getJSONArray("results");
+                    for (int i = 0; i < moviesJson.length(); i++) {
+                        Movie movie = new Movie(moviesJson.getJSONObject(i));
+                        movies.add(movie);
+                        adapter.addAll(movies);
+                        adapter.notifyDataSetChanged();
+                        //movies = Movie.fromJSONArray(moviesJson);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
